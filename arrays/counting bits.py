a2ead7bddf3c7dfb0b2e3646b18b1ced1e6be595
Karan8624 <karanpansare86@gmail.com>
2026-07-26 16:24:40 +0530

    counting bit

---
+++ arrays/counting bits.py
@@ -0,0 +1,16 @@
+def countBits(n):
+    """
+    :type n: int
+    :rtype: List[int]
+    """
+    ans = [0]
+
+    for i in range(1,n+1):
+        if i%2 == 1:
+            ans.append(ans[i-1] +1)
+
+        else:
+            ans.append(ans[i//2])
+
+    return ans
+print(countBits(n = 8))
