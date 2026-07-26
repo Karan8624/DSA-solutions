d4e8492aedb515ecfe1c126e1a379fe3b5ec6992
Karan8624 <karanpansare86@gmail.com>
2026-07-26 15:08:23 +0530

    binary addition

--- arrays/add binary.py
+++ arrays/add binary.py
@@ -1,3 +1,24 @@
 def addBinary(a, b):
-    if
-print(addBinary(a = "1011" , b= "hi")) 
+    alen = len(a) -1
+    blen = len(b) -1 
+    c = 0
+    ans = []
+    while alen >=0 or blen >= 0 or c:
+        sum = c
+        if alen>= 0:
+            sum+= int(a[alen])
+            alen-=1
+        if blen>= 0:
+            sum+= int(b[blen])
+            blen-=1
+
+        ans.append(sum%2)
+        c = sum//2
+    return ''.join(map(str, ans[::-1]))
+
+    
+        
+        
+
+print(addBinary(a = "1010", b = "1011"))
+    
