7bbbc4074cc1a0c425589ff15897852070da1ea8
Karan8624 <karanpansare86@gmail.com>
2026-07-30 00:41:57 +0530

    Gas station problem

---
+++ arrays/Gas station.py
@@ -0,0 +1,68 @@
+def canCompleteCircuit(gas, cost):
+    """
+    :type gas: List[int]
+    :type cost: List[int]
+    :rtype: int
+    """
+    #This is a valid attempt but failed on time complexity since its a O(n2) approach
+    """for i in range(len(gas)):
+        fuel = gas[i] - cost[i]
+        mark = i 
+        while  fuel >= 0:
+            if i == len(gas)-1:
+                i = 0
+            else:
+                i+=1
+            fuel += gas[i] - cost[i]
+            if fuel <0:
+                break
+            else:
+                if mark == i:
+                    return mark
+                continue
+
+    return -1"""
+    # 2nd attempt failed too 
+    """i = 0
+    while i < len(gas):
+        fuel = gas[i] - cost[i]
+        mark = i
+        
+        while fuel >= 0:
+            if i == len(gas)-1:
+                i = 0
+            else:
+                i += 1
+            fuel += gas[i] - cost[i]
+            if fuel < 0:
+                break
+            if mark == i:
+                return mark
+        i = i + 1"""
+    total  = 0
+    curr = 0
+    j = 0
+
+    for i in range(len(gas)):
+        total += gas[i] - cost[i]
+        curr += gas[i] - cost[i]
+
+        if curr < 0:
+            j = i+1
+            curr = 0
+
+    if total > 0:
+        return j
+    else:
+        return -1
+
+    
+        
+
+
+print(canCompleteCircuit(gas = [1,2,3,4,5], cost = [3,4,5,1,2]))
+
+
+            
+
+    
